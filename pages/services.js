2ba2401bd594bc78496c152d8399cef99c61a58d
Nexis-AI <sw.ventures.12@gmail.com>
2026-03-07 19:15:25 -0500

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -21,7 +21,7 @@ export default function Services() {
           </p>
 
           <a
-            href="https://github.com/Infinite-Ais/Titanova-AI/releases/latest/download/clamav-installer.exe.exe"
+            href="https://github.com/Infinite-AIs/Titanova-AI/releases/tag/titanova-scanner"
             target="_blank"
             rel="noopener noreferrer"
           >
