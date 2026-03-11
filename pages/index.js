2d1218adc3ba7c0bd074dbcdfb2c36fde1fde4f6
Nexis-AI <sw.ventures.12@gmail.com>
2026-03-10 20:31:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useState, useRef, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+
 export default function Home() {
   const router = useRouter(); // ✅ Declare router first
   const { data: session } = useSession();
