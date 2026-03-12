c7b8e269d86e99df3769b4d837071f20fe3e237b
Nexis-AI <sw.ventures.12@gmail.com>
2026-03-12 18:33:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useRef, useEffect } from "react";
 import Head from "next/head";
 import { initializeApp } from "firebase/app";
